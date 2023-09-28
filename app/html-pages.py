a498ae372576fd953768dfb16789c82b77800fc4
Piotr Palka <piotr.palka76@gmail.com>
2023-09-29 00:44:07 +0200

    added page

---
+++ app/html-pages.py
@@ -0,0 +1,45 @@
+import os
+
+svg_dir = "icons"  # replace with the path to your directory containing SVGs and subdirectories.
+output_dir = "pages"  # Output directory for the generated web pages.
+
+
+def create_index_page(sub_pages):
+    index_content = "<!DOCTYPE html>\n<html lang='en'>\n<head>\n<meta charset='UTF-8'>\n<title>Index</title>\n</head>\n<body>\n"
+    index_content += "<h1>Index Page</h1>\n<ul>\n"
+    for page in sub_pages:
+        relative_page_path = os.path.join(output_dir, page)
+        index_content += f"<li><a href='{relative_page_path}'>{page}</a></li>\n"
+    index_content += "</ul>\n</body>\n</html>"
+    with open("index.html", "w") as f:
+        f.write(index_content)
+
+
+def create_sub_page(directory, svgs):
+    page_name = directory.replace(os.path.sep, "_") + ".html"
+    page_content = "<!DOCTYPE html>\n<html lang='en'>\n<head>\n<meta charset='UTF-8'>\n<title>{}</title>\n</head>\n<body>\n".format(
+        page_name)
+    page_content += f"<h1>{directory}</h1>\n<ul>\n"
+    for svg in svgs:
+        relative_svg_path = os.path.relpath(os.path.join(directory, svg), output_dir)
+        page_content += f"<li><img src='{relative_svg_path}' alt='{svg}' title='{svg}'/></li>\n"
+    page_content += "</ul>\n</body>\n</html>"
+    with open(os.path.join(output_dir, page_name), "w") as f:
+        f.write(page_content)
+    return page_name
+
+
+def main():
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    sub_pages = []
+    for subdir, _, files in os.walk(svg_dir):
+        svgs = [file for file in files if file.endswith(".svg")]
+        if svgs:
+            rel_dir = os.path.relpath(subdir, svg_dir)
+            sub_pages.append(create_sub_page(rel_dir, svgs))
+    create_index_page(sub_pages)
+
+
+if __name__ == "__main__":
+    main()
